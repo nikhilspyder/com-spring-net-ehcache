63ca08b3ffba5b46862e161f8f68dd21fc3ea6dc
ng185105 <ng185105@ncr.com>
2021-09-18 02:15:24 +0530

    Implementing net EhCache with Spring

--- src/main/java/com/spring/netehache/controller/StudentController.java
+++ src/main/java/com/spring/netehache/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.spring.netehache.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.netehache.domain.Student;
 import com.spring.netehache.service.StudentService;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping(path = "/v1")
 public class StudentController {
 
